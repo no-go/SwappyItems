c23d2b9d49b08e754ea2911f8a00e4a99aaa8336
jochen@homeland <no-go@localhost>
2020-02-17 14:39:11 +0100

    stxxl map is terrible slow in memory only mode

--- STxxL_Map.cpp
+++ STxxL_Map.cpp
@@ -16,49 +16,25 @@ using namespace std;
 //#define MAX_NODES     2405 814 605.0 // EUROPE
 //#define MAX_NODES       67 853 945.0 // NRW (Node mit 18byte sollte dann 1,2 GB RAM brauchen)
 
-
-#define FILE_ITEMS  32*1024
-#define FILE_MULTI        4
-#define RAM_MULTI         2
-
 #define DATA_NODE_BLOCK_SIZE (4096)
 #define DATA_LEAF_BLOCK_SIZE (4096)
 
-typedef uint64_t Key; // for the key-value tuple, 8 byte
-
-struct Value {
-    bool _town;    // 1byte
-    uint8_t _uses; // 1byte
-    double _lon;   // 8byte
-    double _lat;   // 8byte
-    
-    friend std::ostream & operator<<(std::ostream & out, Value const & val) {
-        out << val._lon;
-        return out;
-    }
-};
-
-void ValueSet (Value & v, double lon = 0, double lat = 0, bool town = false, uint8_t uses = 0) {
-    v._uses = uses;
-    v._lon = lon;
-    v._lat = lat;
-    v._town = town;
-}
-
-
-bool stxxlIsEmpty = true;
-
-
-
 struct CompareGreater {
-    bool operator () (const Key& a, const Key& b) const { return a > b; }
-    static Key min_value() { return numeric_limits<Key>::min(); }
-    static Key max_value() { return numeric_limits<Key>::max(); }
+ bool operator () (const uint64_t& a, const uint64_t& b) const
+ { return a > b; }
+ static uint64_t max_value()
+ { return std::numeric_limits<uint64_t>::min(); }
 };
 
-typedef stxxl::map<
- Key, Value, CompareGreater, DATA_NODE_BLOCK_SIZE, DATA_LEAF_BLOCK_SIZE
-> KVstore;
+typedef stxxl::map<uint64_t, bool   , CompareGreater, DATA_NODE_BLOCK_SIZE, DATA_LEAF_BLOCK_SIZE> STXXLtown;
+typedef stxxl::map<uint64_t, uint8_t, CompareGreater, DATA_NODE_BLOCK_SIZE, DATA_LEAF_BLOCK_SIZE> STXXLuses;
+typedef stxxl::map<uint64_t, double , CompareGreater, DATA_NODE_BLOCK_SIZE, DATA_LEAF_BLOCK_SIZE> STXXL_lon;
+typedef stxxl::map<uint64_t, double , CompareGreater, DATA_NODE_BLOCK_SIZE, DATA_LEAF_BLOCK_SIZE> STXXL_lat;
+
+STXXLtown _town((STXXLtown::node_block_type::raw_size) * 3, (STXXLtown::leaf_block_type::raw_size) * 3);
+STXXLuses _uses((STXXLuses::node_block_type::raw_size) * 3, (STXXLuses::leaf_block_type::raw_size) * 3);
+STXXL_lon _lon((STXXL_lon::node_block_type::raw_size) * 3, (STXXL_lon::leaf_block_type::raw_size) * 3);
+STXXL_lat _lat((STXXL_lat::node_block_type::raw_size) * 3, (STXXL_lat::leaf_block_type::raw_size) * 3);
 
 // ---------------------------------------------------------------------
 
@@ -66,36 +42,37 @@ time_t start;
 double seconds;
 
 struct Routing {
-    KVstore * ways;
 
     void node_callback (uint64_t osmid, double lon, double lat, const Tags & tags) {}
 
     void way_callback (uint64_t osmid, const Tags & tags, const vector<uint64_t> & refs) {
         if (tags.find("highway") != tags.end()) {
-            KVstore::iterator wa = ways->end();
+            
             for (uint64_t ref : refs) {
 
-                if (!stxxlIsEmpty) wa = ways->find(ref);
+                STXXLuses::iterator wa = _uses.find(ref);
 
-                if (ways->size()%2048 == 0) {
+                if (_uses.size()%2048 == 0) {
                     time_t now = time(nullptr);
                     seconds = difftime(now,start);
                     printf(
                         "%10.f "
-                        "%10lld ",
+                        "%10lld \n",
                         seconds,
-                        ways->size()
+                        _uses.size()
                     );
-                    ways->print_statistics(std::cout);
+                    //_uses.print_statistics(std::cout);
                 }
                 
-                if (wa == ways->end()) {
-                    stxxlIsEmpty = false;
-                    Value dummy;
-                    ValueSet(dummy, 0.0, 0.0, false);
-                    ways->insert(make_pair(ref, dummy));
+                if (wa == _uses.end()) {
+                    _town.insert(make_pair(ref, false));
+                    _uses.insert(make_pair(ref, 0));
+                    _lon.insert(make_pair(ref, 0.0));
+                    _lat.insert(make_pair(ref, 0.0));
+                    
                 } else {
-                    wa->second._uses++;
+                    //printf(".");
+                    (wa->second)++;
                 }
 
             }
@@ -114,13 +91,8 @@ int main(int argc, char** argv) {
     stxxl::config * cfg = stxxl::config::get_instance();
     cfg->add_disk( stxxl::disk_config("disk=/tmp/stxxl-SwappyItems.tmp, 4 GiB,memory") );
 
-    KVstore * ways = new KVstore(
-        (KVstore::node_block_type::raw_size)*3,
-        (KVstore::leaf_block_type::raw_size)*3
-    );
     Routing routing;
-    routing.ways = ways;
-    
+                    
     start = time(nullptr);
 
     //read_osm_pbf(argv[1], routing, false); // initial read nodes (and relations)
@@ -130,7 +102,6 @@ int main(int argc, char** argv) {
     seconds = difftime(now,start);
     printf("end: %.f\n", seconds);
     
-    delete ways;
     return 0;
 }
 

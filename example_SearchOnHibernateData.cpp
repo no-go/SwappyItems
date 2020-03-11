cb990cf2ccdc7d41603daf8e8e49b058f0334ed6
jochen@homeland <no-go@localhost>
2020-03-11 12:40:07 +0100

    example Search on Hibernate is fixed.

--- example_SearchOnHibernateData.cpp
+++ example_SearchOnHibernateData.cpp
@@ -16,42 +16,34 @@ using namespace std;
 
 typedef uint64_t Key; // for the key-value tuple, 8 byte
 
-struct Value {
+struct WayData {
+    uint8_t _used;
     uint8_t _type;
-    uint8_t _uses;
-    double _lon;
-    double _lat;
-    Key _parent; // if we want to store the whole ref list, then hiberate and file swap must be modified for collection vars !
     char _name[256];
 };
 
-void ValueSet (Value & v, Key parent = 0, double lon = 0.0, double lat = 0.0, uint8_t typ = 0, uint8_t uses = 0) {
-    v._uses = uses;
-    v._lon = lon;
-    v._lat = lat;
-    v._type = typ;
-    v._parent = parent;
-}
+typedef SwappyItems<Key, WayData, FILE_ITEMS, FILE_MULTI, RAM_MULTI, BBITS, BMASK> SwappyItemsWAYS;
+SwappyItemsWAYS * ways;
 
-typedef SwappyItems<Key, Value, FILE_ITEMS, FILE_MULTI, RAM_MULTI, BBITS, BMASK> KVstore;
+
+// ---------------------------------------------------------------------
 
 std::chrono::time_point<std::chrono::system_clock> start;
 double mseconds;
 
 int main(int argc, char** argv) {
-    Key query = 313617373; // 428124430 // 250072529
+    Key query = 103535041; // 185769837
     if (argc == 2) query = stol(argv[1]);
     start = std::chrono::high_resolution_clock::now();
 
-    KVstore * ways = new KVstore(23);
-    std::pair<Value, vector<Key> > * val = ways->get(query);
+    SwappyItemsWAYS * ways = new SwappyItemsWAYS(23);
+    std::pair<WayData, vector<Key> > * val = ways->get(query);
     
     if (val == nullptr) {
         printf("The '%lu' does not exist.\n", query);
     } else {
         printf("Name of '%lu': %s\n", query, val->first._name);
-        printf("  lon,lat: %lf, %lf\n", val->first._lon, val->first._lat);
-        printf("     uses: %d\n", val->first._uses);
+        printf("     uses: %d\n", val->first._used);
     }
     
     delete ways;

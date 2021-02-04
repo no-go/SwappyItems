b9f67ad3f3237579671afe74c028101265ba7d70
jochen@homeland <no-go@localhost>
2021-02-04 20:42:46 +0100

    first hint: the elemet 18 with zero prio exists in the system 2 times, if we get it with topgit add .

--- examples/TwoPhasePairPrioQ/main.cpp
+++ examples/TwoPhasePairPrioQ/main.cpp
@@ -15,23 +15,35 @@ int main(int argc, char** argv) {
         pq.set(rand()%200, e);
         pq.del(rand()%200);
     }
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 200; ++i) {
         TwoPhasePairingHeap<uint64_t>::Element e;
         exi = pq.get(i, e);
         if (exi) printf("%5d prio: %3d data: %12d\n", i, e.prio, e.data);
     }
-    for (int i = 0; i < 20; ++i) {
+    printf("\nrun top() now!\n\n");
+    for (int i = 0; i < 200; ++i) {
         TwoPhasePairingHeap<uint64_t>::Element e;
         exi = pq.top(k, e);
         if (exi == false) {
             i = 200;
         } else {
-            printf("%5d %12d %3d\n", i, k, e.prio);
+            printf("%5d prio: %3d data: %12d\n", k, e.prio, e.data);
             if (e.prio == 0) printf("zero prio should not happend!\n");
             pq.del(k);
         }
     }
     return 0;
 }
-
-
+// Index 18 exists 2 times !?
+//
+//run top() now!
+//
+//  103 prio:   5 data:    767066249
+//   47 prio:   5 data:   1010528946
+//   18 prio:   5 data:    333582338
+//   18 prio:   0 data:            0
+//zero prio should not happend!
+//   98 prio:   5 data:     19485054
+//   92 prio:   5 data:    452456682
+//   93 prio:   5 data:    765326717
+//...
